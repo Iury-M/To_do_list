cab77aff8c3ce28a5697e57db022967ed771b52d
Iury-M <iuryprimeirol@gmail.com>
2025-07-17 19:08:10 -0300

    refactor: Atualiza para AWS SDK v3 e corrige upload

--- backend/uploads.js
+++ backend/uploads.js
@@ -1,40 +1,47 @@
-const aws = require('aws-sdk');
 const multer = require('multer');
-const multerS3 = require('multer-s3');
 const path = require('path');
+const { S3Client } = require('@aws-sdk/client-s3');
+const multerS3 = require('multer-s3');
 
-// Configura a AWS apenas se as variáveis de ambiente existirem
-if (process.env.AWS_ACCESS_KEY_ID) {
-  aws.config.update({
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    region: process.env.AWS_REGION
+let s3, storage;
+
+// Configura a AWS S3 apenas se as variáveis de ambiente existirem
+if (process.env.AWS_BUCKET_NAME) {
+  s3 = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    }
+  });
+
+  // Storage para a S3
+  storage = multerS3({
+    s3: s3,
+    bucket: process.env.AWS_BUCKET_NAME,
+    acl: 'public-read', // Permite que os ficheiros sejam vistos publicamente
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      const ext = path.extname(file.originalname);
+      const uniqueName = `task-file-${Date.now()}${ext}`;
+      cb(null, uniqueName);
+    }
+  });
+} else {
+  // Storage local (para quando estiver a desenvolver no seu computador)
+  storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+      const ext = path.extname(file.originalname);
+      const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1E9) + ext;
+      cb(null, uniqueName);
+    }
   });
 }
-
-const s3 = new aws.S3();
-
-const storage = process.env.AWS_BUCKET_NAME
-  ? multerS3({
-      s3: s3,
-      bucket: process.env.AWS_BUCKET_NAME,
-      acl: 'public-read', // Permite que os ficheiros sejam vistos publicamente
-      key: function (req, file, cb) {
-        const ext = path.extname(file.originalname);
-        const uniqueName = `task-file-${Date.now()}${ext}`;
-        cb(null, uniqueName);
-      }
-    })
-  : multer.diskStorage({
-      destination: function (req, file, cb) {
-        cb(null, 'uploads/');
-      },
-      filename: function (req, file, cb) {
-        const ext = path.extname(file.originalname);
-        const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1E9) + ext;
-        cb(null, uniqueName);
-      }
-    });
 
 const upload = multer({ storage: storage });
 
